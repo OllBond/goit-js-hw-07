e81f0fdb53f9734637b7eeaa962c2bd525f0a995
Olena <newsroommy@gmail.com>
2022-11-08 15:10:30 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
 
-function createPicturesMarkup (pictures) {
+const galleryContainer = document.querySelector('.gallery');
 
-const markup = pictures.map(picture => {
-return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+const picturesMarkup = createPicturesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+function createPicturesMarkup (pictures) {
+return pictures.map(({preview, original, description}) => {
+return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>
 `;
-});
-
-console.log(markup);
+})
+.join(''); 
 }
 
+// console.log(createPicturesMarkup);
